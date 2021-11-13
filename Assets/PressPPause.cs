babff2ebb5074c08fb0b40b2b22feb66fe8d39bc
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-11-13 11:37:03 -0600

    added pausing

---
+++ Assets/PressPPause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressPPause : MonoBehaviour
+{
+
+    public UnityEvent pauseAction;
+
+    void Update()
+    {
+
+    }
+}
