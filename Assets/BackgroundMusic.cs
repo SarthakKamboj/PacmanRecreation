ea1fae12c5fd4e920d7e9852cc0d72f45964903e
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-11-13 11:08:54 -0600

    added background music

---
+++ Assets/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject[] backgroundMusics = GameObject.FindGameObjectsWithTag("BackgroundMusic");
+        if (backgroundMusics.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
